bd45c044b4ce30ce902c2d46298d289b605436ff
jtarrio <jtarrio@localhost>
2012-02-25 22:51:40 -0800

    Add AllTests test suite.

---
+++ src/test/java/org/tarrio/dilate/AllTests.java
@@ -0,0 +1,19 @@
+package org.tarrio.dilate;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		//$JUnit-BEGIN$
+		suite.addTestSuite(EndToEndTest.class);
+		suite.addTestSuite(LookupBufferTest.class);
+		suite.addTestSuite(Lz77CompressorTest.class);
+		suite.addTestSuite(XmlCodecTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
